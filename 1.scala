4dae3b36c04d799dd9c6267c67eb2e081ca0d7f5
MadushariniRanasinghe12 <87421056+MadushariniRanasinghe12@users.noreply.github.com>
2022-09-13 00:15:58 +0530

    Update 1.scala

--- 1.scala
+++ 1.scala
@@ -2,8 +2,8 @@ object Q1 extends App {
     val obj1 = new Rational(1,2)
     val obj2 = new Rational(4,5)
     println(obj1.neg)
-    val q = obj1.add(obj2)
-    println(q)
+    println(obj2.neg)
+    
 }
 
 class Rational(n:Int, d:Int) {
@@ -11,7 +11,7 @@ class Rational(n:Int, d:Int) {
     def numer = n
     def denom = d
 
-    def add(r:Rational) = new Rational(this.numer*r.denom + this.denom*r.numer, this.denom*r.denom)
     def neg = new Rational(-this.numer, this.denom)
     override def toString(): String = numer + "/" + denom
+    
 }
